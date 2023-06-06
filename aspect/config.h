d2831e8b8064401ba3e596a602bf9fb25013b063
Yaspect-ux <2228217372@qq.com>
2023-06-06 11:28:48 -0700

    stl_config

--- aspect/config.h
+++ aspect/config.h
@@ -7,6 +7,12 @@
 #include <boost/lexical_cast.hpp>
 #include <yaml-cpp/yaml.h>
 #include "aspect/log.h"
+#include <list>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
 
 namespace aspect{
     
@@ -41,8 +47,205 @@ public:
     }
 };
 
+/**
+ * @brief 读取yaml的vector
+*/
+//for vector:利用类模板通过重写（）实现stl与string的相互转换
+template<class T>
+class LexicalCast<std::string,std::vector<T>> {
+public: //c++ struct默认访问权限public,class默认为private
+    std::vector<T> operator() (const std::string& v) {
+        YAML::Node node = YAML::Load(v);
+        typename std::vector<T> vec;
+        std::stringstream ss;
+        for (size_t i = 0; i < node.size(); ++i) {
+            ss.str("");
+            ss << node[i];
+            vec.push_back(LexicalCast<std::string,T>() (ss.str()) );
+        }
+        return vec;
+    }
+};
+/**
+ * @brief 向yaml写入vector
+*/
+template<class T>
+class LexicalCast<std::vector<T>,std::string> {
+public:
+     std::string operator() (const std::vector<T>& v) {
+        YAML::Node node;
+        for (auto& i : v) {
+            node.push_back(YAML::Load(LexicalCast<T,std::string>()(i)));//这里不会造成类型与类内静态的二义性不用typename
+        }
+        std::stringstream ss;
+        ss << node;
+        return ss.str();
+    }
+};
+
+/**
+ * @brief 读取yaml的list
+*/
+template<class T>
+class LexicalCast<std::string,std::list<T>> {
+public: //c++ struct默认访问权限public,class默认为private
+    std::list<T> operator() (const std::string& v) {
+        YAML::Node node = YAML::Load(v);
+        typename std::list<T> vec;
+        std::stringstream ss;
+        for (size_t i = 0; i < node.size(); ++i) {
+            ss.str("");
+            ss << node[i];
+            vec.push_back(LexicalCast<std::string,T>() (ss.str()) );
+        }
+        return vec;
+    }
+};
+/**
+ * @brief 向yaml写入list
+*/
+template<class T>
+class LexicalCast<std::list<T>,std::string> {
+public:
+     std::string operator() (const std::list<T>& v) {
+        YAML::Node node;
+        for (auto& i : v) {
+            node.push_back(YAML::Load(LexicalCast<T,std::string>()(i)));//这里不会造成类型与类内静态的二义性不用typename
+        }
+        std::stringstream ss;
+        ss << node;
+        return ss.str();
+    }
+};
+
+// for set
+template<class T>
+class LexicalCast<std::string,std::set<T>> {
+public: //c++ struct默认访问权限public,class默认为private
+    std::set<T> operator() (const std::string& v) {
+        YAML::Node node = YAML::Load(v);
+        typename std::set<T> vec;
+        std::stringstream ss;
+        for (size_t i = 0; i < node.size(); ++i) {
+            ss.str("");
+            ss << node[i];
+            vec.insert(LexicalCast<std::string,T>() (ss.str()) );
+        }
+        return vec;
+    }
+};
+template<class T>
+class LexicalCast<std::set<T>,std::string> {
+public:
+     std::string operator() (const std::set<T>& v) {
+        YAML::Node node;
+        for (auto& i : v) {
+            node.push_back(YAML::Load(LexicalCast<T,std::string>()(i)));//这里不会造成类型与类内静态的二义性不用typename
+        }
+        std::stringstream ss;
+        ss << node;
+        return ss.str();
+    }
+};
+
+// for unordered_set
+template<class T>
+class LexicalCast<std::string,std::unordered_set<T>> {
+public: //c++ struct默认访问权限public,class默认为private
+    std::unordered_set<T> operator() (const std::string& v) {
+        YAML::Node node = YAML::Load(v);
+        typename std::unordered_set<T> vec;
+        std::stringstream ss;
+        for (size_t i = 0; i < node.size(); ++i) {
+            ss.str("");
+            ss << node[i];
+            vec.insert(LexicalCast<std::string,T>() (ss.str()) );
+        }
+        return vec;
+    }
+};
+template<class T>
+class LexicalCast<std::unordered_set<T>,std::string> {
+public:
+     std::string operator() (const std::unordered_set<T>& v) {
+        YAML::Node node;
+        for (auto& i : v) {
+            node.push_back(YAML::Load(LexicalCast<T,std::string>()(i)));//这里不会造成类型与类内静态的二义性不用typename
+        }
+        std::stringstream ss;
+        ss << node;
+        return ss.str();
+    }
+};
+
+// for map
+template<class T>
+class LexicalCast<std::string,std::map<std::string,T>> {
+public: //c++ struct默认访问权限public,class默认为private
+    std::map<std::string,T> operator() (const std::string& v) {
+        YAML::Node node = YAML::Load(v);
+        typename std::map<std::string,T> vec;
+        std::stringstream ss;
+        for (auto it = node.begin();
+            it != node.end(); ++it) {
+            ss.str("");
+            ss << it->second;
+            vec.insert(std::make_pair(it->first.Scalar(),
+                LexicalCast<std::string,T>() (ss.str()) ) );
+        }
+        return vec;
+    }
+};
+template<class T>
+class LexicalCast<std::map<std::string,T>,std::string> {
+public:
+     std::string operator() (const std::map<std::string,T>& v) {
+        YAML::Node node;
+        for (auto& i : v) {
+            node[i.first] = YAML::Load(LexicalCast<T,std::string>()(i.second));
+        }
+        std::stringstream ss;
+        ss << node;
+        return ss.str();
+    }
+};
+
+// for unordered_map
+template<class T>
+class LexicalCast<std::string,std::unordered_map<std::string,T>> {
+public: //c++ struct默认访问权限public,class默认为private
+    std::unordered_map<std::string,T> operator() (const std::string& v) {
+        YAML::Node node = YAML::Load(v);
+        typename std::unordered_map<std::string,T> vec;
+        std::stringstream ss;
+        for (auto it = node.begin();
+            it != node.end(); ++it) {
+            ss.str("");
+            ss << it->second;
+            vec.insert(std::make_pair(it->first.Scalar(),
+                LexicalCast<std::string,T>() (ss.str()) ) );
+        }
+        return vec;
+    }
+};
+template<class T>
+class LexicalCast<std::unordered_map<std::string,T>,std::string> {
+public:
+     std::string operator() (const std::unordered_map<std::string,T>& v) {
+        YAML::Node node;
+        for (auto& i : v) {
+            node[i.first] = YAML::Load(LexicalCast<T,std::string>()(i.second));
+        }
+        std::stringstream ss;
+        ss << node;
+        return ss.str();
+    }
+};
+
+
+
 template<class T,class FromStr = LexicalCast<std::string,T>,
-                class ToStr = LexicalCast<T,std::string> >
+                class ToStr = LexicalCast<T,std::string>>
 class ConfigVar : public ConfigVarBase{
 public:
     typedef std::shared_ptr<ConfigVar> ptr;
--- tests/test_config.cc
+++ tests/test_config.cc
@@ -9,6 +9,23 @@ aspect::ConfigVar<int>::ptr g_int_value_config =
 aspect::ConfigVar<float>::ptr g_float_value_config =
     aspect::Config::Lookup("system.value", (float)10.2f, "system value");
 
+aspect::ConfigVar<std::vector<int>>::ptr g_int_vec_value_config =
+    aspect::Config::Lookup("system.int_vec", std::vector<int>{1,2}, "system int vec");
+
+aspect::ConfigVar<std::list<int>>::ptr g_int_list_value_config =
+    aspect::Config::Lookup("system.int_list", std::list<int>{1,2}, "system int list");
+
+aspect::ConfigVar<std::set<int>>::ptr g_int_set_value_config =
+    aspect::Config::Lookup("system.int_set", std::set<int>{1,2}, "system int set");
+
+aspect::ConfigVar<std::unordered_set<int>>::ptr g_int_unordered_set_value_config =
+    aspect::Config::Lookup("system.int_uset", std::unordered_set<int>{1,2}, "system int unordered_set");
+
+aspect::ConfigVar<std::map<std::string,int>>::ptr g_str_int_map_value_config =
+    aspect::Config::Lookup("system.str_int_map", std::map<std::string,int>{{"k",2}}, "system str int map");
+
+aspect::ConfigVar<std::unordered_map<std::string,int>>::ptr g_str_int_unordered_map_value_config =
+    aspect::Config::Lookup("system.str_int_umap", std::unordered_map<std::string,int>{{"k",2}}, "system str int unordered_map");
 void print_yaml(const YAML::Node& node, int level) {
     if(node.IsScalar()) {
         ASPECT_LOG_INFO(ASPECT_LOG_ROOT()) << std::string(level * 4, ' ')
@@ -45,11 +62,44 @@ void test_config()
     ASPECT_LOG_INFO(ASPECT_LOG_ROOT()) << "before:" << g_int_value_config->getValue();
     ASPECT_LOG_INFO(ASPECT_LOG_ROOT()) << "before:" << g_float_value_config->toString();
 
+#define XX(g_var, name, prefix) \
+    { \
+        auto&v = g_var->getValue(); \
+        for (auto& i : v) { \
+            ASPECT_LOG_INFO(ASPECT_LOG_ROOT()) << #prefix ":" #name ":" << i; \
+        } \
+        ASPECT_LOG_INFO(ASPECT_LOG_ROOT()) << #prefix ":" #name " yaml:" << g_var->toString(); \
+    }
+#define XX_M(g_var, name, prefix) \
+    { \
+        auto&v = g_var->getValue(); \
+        for (auto& i : v) { \
+            ASPECT_LOG_INFO(ASPECT_LOG_ROOT()) << #prefix ":" #name ": {" \
+                            << i.first << " - " << i.second << "}"; \
+        }; \
+        ASPECT_LOG_INFO(ASPECT_LOG_ROOT()) << #prefix ":" #name " yaml:" << g_var->toString(); \
+    }
+
+    XX(g_int_vec_value_config,int_vec,before);
+    XX(g_int_list_value_config,int_list,before);
+    XX(g_int_set_value_config,int_set,before);
+    XX(g_int_unordered_set_value_config,int_unordered_set,before);
+    XX_M(g_str_int_map_value_config,str_int_map,before);
+    XX_M(g_str_int_unordered_map_value_config,str_int_umap,before);
+
     YAML::Node root = YAML::LoadFile("/home/linno/Desktop/Project/AspectServer/bin/conf/log.yml");
     aspect::Config::LoadFromYaml(root);
 
     ASPECT_LOG_INFO(ASPECT_LOG_ROOT()) << "after:" << g_int_value_config->getValue();
     ASPECT_LOG_INFO(ASPECT_LOG_ROOT()) << "after:" << g_float_value_config->toString();
+
+    XX(g_int_vec_value_config,int_vec,after);
+    XX(g_int_list_value_config,int_list,after);
+    XX(g_int_set_value_config,int_set,after);
+    XX(g_int_unordered_set_value_config,int_unordered_set,after);
+    XX_M(g_str_int_map_value_config,str_int_map,after);
+    XX_M(g_str_int_unordered_map_value_config,str_int_umap,after);
+
 }
 
 int main(int argc,char** argv)
